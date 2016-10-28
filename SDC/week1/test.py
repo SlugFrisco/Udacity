a8606f3d215a0c77896f3e34d031366aa96e4b44
Casey <c.juanxi.li@gmail.com>
2016-10-27 21:35:14 -0400

    Create SDC folder and add first quiz

---
+++ SDC/week1/test.py
@@ -0,0 +1,35 @@
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+import numpy as np
+
+# Read in the image and print out some stats
+# "image" is an instance of the mpimg class from matplotlib
+image = mpimg.imread('test.jpg')
+print('This image is: ',type(image),
+         'with dimensions:', image.shape)
+
+# Grab the x and y size and make a copy of the image
+ysize = image.shape[0]
+xsize = image.shape[1]
+# this makes a copy of the image object
+color_select = np.copy(image)
+
+# Define our color selection criteria
+red_threshold = 200
+green_threshold = 200
+blue_threshold = 200
+rgb_threshold = [red_threshold, green_threshold, blue_threshold]
+
+# Use a "bitwise OR" to identify pixels below the threshold
+# in python the pipe is bitwise OR
+# in english: IF there is less than this much red, OR this much green, OR this much blue, put it in "thresholds"
+thresholds = (image[:,:,0] < rgb_threshold[0]) \
+            | (image[:,:,1] < rgb_threshold[1]) \
+            | (image[:,:,2] < rgb_threshold[2])
+
+# then set the pixels in "thresholds" to black
+color_select[thresholds] = [0,0,0]
+
+# Display the image
+plt.imshow(color_select)
+plt.show()
